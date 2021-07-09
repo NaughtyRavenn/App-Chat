f1a2d1f8dc9184d4c20bea309a88115a55b65ed1
NaughtyRavenn <bachminh.uit@gmail.com>
2021-07-09 09:01:26 +0700

    Add avatar

---
+++ UpdateImage/UpdateImage/DataProvider.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpdateImage
+{
+    class DataProvider
+    {
+        private static DataProvider instance;
+        internal static DataProvider Instance
+        {
+            get
+            {
+                if (DataProvider.instance == null)
+                    instance = new DataProvider();
+                return DataProvider.instance;
+            }
+            private set
+            {
+                DataProvider.instance = value;
+            }
+        }
+
+        public object DataTable { get; private set; }
+
+        public int x, y;
+        public int Getx() { return x; }
+        public void Setx(int x) { this.x = x; }
+        public int Gety() { return y; }
+        public void Sety(int y) { this.y = y; }
+
+
+        private DataProvider() { }
+
+        protected static SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(GetConnectionString());
+        private static string GetConnectionString()
+        {
+            return @"Data Source=LAPTOP-L3JT52S4;Initial Catalog=ChatTest;Integrated Security=True";
+        }
+
+        [DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int conn, int val);
+
+        public DataTable ExcuteQuery(string query, object[] parameters = null)
+        {
+            DataTable DataProviderTable = new DataTable();
+            int Out;
+            if (InternetGetConnectedState(out Out, 0))
+            {
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    try
+                    {
+                        connection.Open();
+                    }
+
+                    catch
+                    {
+                        MessageBox.Show("Mất mạng!");
+                        return null;
+                    }
+
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    if (parameters != null)
+                    {
+                        string[] splitPara = query.Split(' ');
+                        int i = 0;
+                        foreach (string item in splitPara)
+                        {
+                            if (item.Contains('@'))
+                            {
+                                command.Parameters.AddWithValue(item, parameters[i]);
+                                i++;
+                            }
+                        }
+                    }
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(DataProviderTable);
+                    connection.Close();
+                }
+            }
+            else
+            {
+                if (MessageBox.Show("Vui lòng khởi động lại chương trình", "Mất kết nối", MessageBoxButtons.OK) == System.Windows.Forms.DialogResult.OK)
+                {
+                    Application.Exit();
+                }
+            }
+            return DataProviderTable;
+        }
+    }
+}
---
+++ UpdateImage/UpdateImage/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpdateImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            byte[] bytes = FromFile(getLinkFromDialog());
+            string sqlQuery = "UPDATE GROUPCHAT SET Avatar = @a ";
+            DataProvider.Instance.ExcuteQuery(sqlQuery, new object[] { bytes });
+
+            byte[] bytes1 = FromFile(getLinkFromDialog());
+            string sqlQuery1 = "UPDATE GROUPCHAT SET Avatar = @a ";
+            DataProvider.Instance.ExcuteQuery(sqlQuery1, new object[] { bytes1 });
+        }
+
+        static public string getLinkFromDialog()
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = "Select avatar";
+            open.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+                "JPEG (.jpg;.jpeg)|*.jpg;*.jpeg|" +
+                "Portable Network Graphic (.png)|.png";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                return open.FileName;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        static public byte[] FromFile(string path)
+        {
+            if (path == "")
+            {
+                return null;
+            }
+            else
+            {
+                var bytes = File.ReadAllBytes(path);
+                return bytes;
+            }
+        }
+    }
+}
